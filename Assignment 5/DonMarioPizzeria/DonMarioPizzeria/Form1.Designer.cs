6e84d8e3d3460f82a677e29b03e4b04edeb4c294
Lyceum <48872358+cab-aranw@users.noreply.github.com>
2020-02-06 00:55:31 -0500

    Missing Calculations
    Missing displaying in textbox

--- Assignment 5/DonMarioPizzeria/DonMarioPizzeria/Form1.Designer.cs
+++ Assignment 5/DonMarioPizzeria/DonMarioPizzeria/Form1.Designer.cs
@@ -43,12 +43,14 @@
             this.checkBoxToppingBacon = new System.Windows.Forms.CheckBox();
             this.checkBoxToppingCheese = new System.Windows.Forms.CheckBox();
             this.listBoxServiceQuality = new System.Windows.Forms.ListBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
             this.labelPrice = new System.Windows.Forms.Label();
             this.textBoxPrice = new System.Windows.Forms.TextBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBoxFood.SuspendLayout();
             this.groupBoxDelivery.SuspendLayout();
@@ -73,9 +75,9 @@
             this.radioButtonFoodPizza.Name = "radioButtonFoodPizza";
             this.radioButtonFoodPizza.Size = new System.Drawing.Size(72, 24);
             this.radioButtonFoodPizza.TabIndex = 1;
-            this.radioButtonFoodPizza.TabStop = true;
             this.radioButtonFoodPizza.Text = "Pizza";
             this.radioButtonFoodPizza.UseVisualStyleBackColor = true;
+            this.radioButtonFoodPizza.CheckedChanged += new System.EventHandler(this.radioButtonFoodPizza_CheckedChanged);
             // 
             // groupBoxFood
             // 
@@ -88,6 +90,7 @@
             this.groupBoxFood.TabIndex = 2;
             this.groupBoxFood.TabStop = false;
             this.groupBoxFood.Text = "Food";
+            this.groupBoxFood.Enter += new System.EventHandler(this.groupBoxFood_Enter);
             // 
             // radioButtonFoodLasagna
             // 
@@ -96,9 +99,9 @@
             this.radioButtonFoodLasagna.Name = "radioButtonFoodLasagna";
             this.radioButtonFoodLasagna.Size = new System.Drawing.Size(96, 24);
             this.radioButtonFoodLasagna.TabIndex = 3;
-            this.radioButtonFoodLasagna.TabStop = true;
             this.radioButtonFoodLasagna.Text = "Lasagna";
             this.radioButtonFoodLasagna.UseVisualStyleBackColor = true;
+            this.radioButtonFoodLasagna.CheckedChanged += new System.EventHandler(this.radioButtonFoodLasagna_CheckedChanged);
             // 
             // radioButtonFoodSpaghetti
             // 
@@ -107,14 +110,15 @@
             this.radioButtonFoodSpaghetti.Name = "radioButtonFoodSpaghetti";
             this.radioButtonFoodSpaghetti.Size = new System.Drawing.Size(103, 24);
             this.radioButtonFoodSpaghetti.TabIndex = 2;
-            this.radioButtonFoodSpaghetti.TabStop = true;
             this.radioButtonFoodSpaghetti.Text = "Spaghetti";
             this.radioButtonFoodSpaghetti.UseVisualStyleBackColor = true;
+            this.radioButtonFoodSpaghetti.CheckedChanged += new System.EventHandler(this.radioButtonFoodSpaghetti_CheckedChanged);
             // 
             // groupBoxDelivery
             // 
             this.groupBoxDelivery.Controls.Add(this.radioButtonDeliveryInStore);
             this.groupBoxDelivery.Controls.Add(this.radioButtonDeliveryToGo);
+            this.groupBoxDelivery.Enabled = false;
             this.groupBoxDelivery.Location = new System.Drawing.Point(232, 146);
             this.groupBoxDelivery.Name = "groupBoxDelivery";
             this.groupBoxDelivery.Size = new System.Drawing.Size(213, 246);
@@ -150,10 +154,11 @@
             this.groupBoxToppings.Controls.Add(this.checkBoxToppingHam);
             this.groupBoxToppings.Controls.Add(this.checkBoxToppingBacon);
             this.groupBoxToppings.Controls.Add(this.checkBoxToppingCheese);
+            this.groupBoxToppings.Enabled = false;
             this.groupBoxToppings.Location = new System.Drawing.Point(21, 409);
             this.groupBoxToppings.Name = "groupBoxToppings";
             this.groupBoxToppings.Size = new System.Drawing.Size(235, 150);
-            this.groupBoxToppings.TabIndex = 4;
+            this.groupBoxToppings.TabIndex = 1;
             this.groupBoxToppings.TabStop = false;
             this.groupBoxToppings.Text = "Toppings";
             // 
@@ -201,25 +206,29 @@
             // listBoxServiceQuality
             // 
             this.listBoxServiceQuality.AllowDrop = true;
+            this.listBoxServiceQuality.Enabled = false;
             this.listBoxServiceQuality.FormattingEnabled = true;
             this.listBoxServiceQuality.ItemHeight = 20;
             this.listBoxServiceQuality.Items.AddRange(new object[] {
             "Very Good",
             "Good",
-            "Average"});
+            "Average",
+            "Poor",
+            "Very Poor"});
             this.listBoxServiceQuality.Location = new System.Drawing.Point(11, 580);
             this.listBoxServiceQuality.Name = "listBoxServiceQuality";
-            this.listBoxServiceQuality.Size = new System.Drawing.Size(433, 84);
+            this.listBoxServiceQuality.Size = new System.Drawing.Size(444, 104);
             this.listBoxServiceQuality.TabIndex = 5;
             // 
-            // button1
+            // buttonOpen
             // 
-            this.button1.Location = new System.Drawing.Point(502, 350);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(86, 42);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonOpen.Location = new System.Drawing.Point(502, 350);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(86, 42);
+            this.buttonOpen.TabIndex = 6;
+            this.buttonOpen.Text = "&Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
             // 
             // labelPrice
             // 
@@ -227,54 +236,68 @@
             this.labelPrice.Location = new System.Drawing.Point(275, 460);
             this.labelPrice.Name = "labelPrice";
             this.labelPrice.Size = new System.Drawing.Size(44, 20);
-            this.labelPrice.TabIndex = 7;
+            this.labelPrice.TabIndex = 4;
             this.labelPrice.Text = "Price";
             // 
             // textBoxPrice
             // 
+            this.textBoxPrice.Enabled = false;
             this.textBoxPrice.Location = new System.Drawing.Point(334, 457);
             this.textBoxPrice.Name = "textBoxPrice";
             this.textBoxPrice.Size = new System.Drawing.Size(121, 26);
-            this.textBoxPrice.TabIndex = 8;
+            this.textBoxPrice.TabIndex = 4;
             // 
-            // button2
+            // buttonSave
             // 
-            this.button2.Location = new System.Drawing.Point(502, 438);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(86, 42);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button1";
-            this.button2.UseVisualStyleBackColor = true;
+            this.buttonSave.Location = new System.Drawing.Point(502, 438);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(86, 42);
+            this.buttonSave.TabIndex = 7;
+            this.buttonSave.Text = "&Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
             // 
-            // button3
+            // buttonClear
             // 
-            this.button3.Location = new System.Drawing.Point(502, 517);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(86, 42);
-            this.button3.TabIndex = 6;
-            this.button3.Text = "button1";
-            this.button3.UseVisualStyleBackColor = true;
+            this.buttonClear.Location = new System.Drawing.Point(502, 517);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(86, 42);
+            this.buttonClear.TabIndex = 8;
+            this.buttonClear.Text = "&Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
             // 
-            // button4
+            // buttonExit
             // 
-            this.button4.Location = new System.Drawing.Point(502, 593);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(86, 42);
-            this.button4.TabIndex = 6;
-            this.button4.Text = "button1";
-            this.button4.UseVisualStyleBackColor = true;
+            this.buttonExit.Location = new System.Drawing.Point(502, 593);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(86, 42);
+            this.buttonExit.TabIndex = 9;
+            this.buttonExit.Text = "&Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.DefaultExt = "txt";
+            this.saveFileDialog1.FileName = "order1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(675, 960);
             this.Controls.Add(this.textBoxPrice);
             this.Controls.Add(this.labelPrice);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonOpen);
             this.Controls.Add(this.listBoxServiceQuality);
             this.Controls.Add(this.groupBoxToppings);
             this.Controls.Add(this.groupBoxDelivery);
@@ -311,12 +334,14 @@
         private System.Windows.Forms.CheckBox checkBoxToppingBacon;
         private System.Windows.Forms.CheckBox checkBoxToppingCheese;
         private System.Windows.Forms.ListBox listBoxServiceQuality;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonOpen;
         private System.Windows.Forms.Label labelPrice;
         private System.Windows.Forms.TextBox textBoxPrice;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
     }
 }
 
--- Assignment 5/DonMarioPizzeria/DonMarioPizzeria/Form1.cs
+++ Assignment 5/DonMarioPizzeria/DonMarioPizzeria/Form1.cs
@@ -12,14 +12,143 @@ namespace DonMarioPizzeria
 {
     public partial class Form1 : Form
     {
+        const int COST_PIZZA = 5;
+        const int COST_SPAGUETTI = 10;
+        const int COST_LASAGNA = 15;
+        const int EXTRA_CHEESE = 2;
+        const int EXTRA_HAM = 2;
+        const int EXTRA_BACON = 2;
+        const int EXTRA_OLIVES = 2;
+        int total = 0;
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            // Clear the selected item in the group box Food
+            groupBoxDelivery.Enabled = false;
+
+            // Clear the selected item in the group box Delivery
+
+            groupBoxDelivery.Enabled = false;
+
+            // Clear the checked item in the groupBox Toppings
+            //checkBoxToppingCheese.Checked = false;
+            //checkBoxToppingBacon.Checked = false;
+            //checkBoxToppingHam.Checked = false;
+            //checkBoxToppingOlives.Checked = false;
+
+            groupBoxToppings.Enabled = false;
+            listBoxServiceQuality.Enabled = false;
+
+            // Clear the checked item in the listBox
+
+            //listBoxServiceQuality.ClearSelected();
+
+            textBoxPrice.Text = " ";
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            //Displays the open file dialog box
+            //openFileDialog1.ShowDialog();
+            String fileName;
+            GetFileName(out fileName);
+        }
+
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            //Displays the safe dialog box
+            saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFileDialog1.ShowDialog();
+            
+        }
+
+        private void GetFileName(out string selectedFile)
+        {
+            if (openFileDialog1.ShowDialog () == DialogResult.OK)
+            {
+                selectedFile = openFileDialog1.FileName;
+            }
+            else
+            {
+                selectedFile = " ";
+            }
+        }
+
+        private void groupBoxFood_Enter(object sender, EventArgs e)
+        {
+            if (radioButtonFoodPizza.Checked == true || radioButtonFoodLasagna.Checked == true || radioButtonFoodLasagna.Checked == true)
+            {
+                groupBoxDelivery.Enabled = true;
+                
+            }
+            else
+            {
+                groupBoxDelivery.Enabled = false;
+            }
+
+        }
+
+        private void radioButtonFoodPizza_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonFoodPizza.Checked)
+            {
+                groupBoxDelivery.Enabled = true;
+                groupBoxToppings.Enabled = true;
+                listBoxServiceQuality.Enabled = true;
+            }
+            else
+            {
+                groupBoxDelivery.Enabled = false;
+                groupBoxToppings.Enabled = false;
+                listBoxServiceQuality.Enabled = false;
+            }
+        }
+
+        private void radioButtonFoodSpaghetti_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonFoodSpaghetti.Checked)
+            {
+                groupBoxDelivery.Enabled = true;
+                groupBoxToppings.Enabled = true;
+                listBoxServiceQuality.Enabled = true;
+            }
+            else
+            {
+                groupBoxDelivery.Enabled = false;
+                groupBoxToppings.Enabled = false;
+                listBoxServiceQuality.Enabled = false;
+            }
+        }
+
+        private void radioButtonFoodLasagna_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioButtonFoodLasagna.Checked)
+            {
+                groupBoxDelivery.Enabled = true;
+                groupBoxToppings.Enabled = true;
+                listBoxServiceQuality.Enabled = true;
+            }
+            else
+            {
+                groupBoxDelivery.Enabled = false;
+                groupBoxToppings.Enabled = false;
+                listBoxServiceQuality.Enabled = false;
+            }
         }
     }
 }
